e9b5123a0020f16f2ff7c84ed70b8b4e96398b8e
itrcvivek <itrcvivek@gmail.com>
2024-07-17 15:04:56 +0530

    s

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const FrequentlyQuestion = require("./routes/FrequentlyQuestionRoutes")
 const Booking = require("./routes/BookingRoutes")
 
 app.use("/", (req, res) => {
-    res.json("Hello, World! I am from backend_aws");
+    res.json({message:"hello from express app"});
 });
 app.use("/api/v1/", user);
 app.use("/api/v1/", category);
